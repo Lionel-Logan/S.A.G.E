733c24899a69e78ac77f586a7a6e3c2e1bdd6b27
Navneet Ranjish Pillai <navneetranjishpillai@gmail.com>
2026-01-21 00:52:23 +0530

    implemented a testing app
    - not important push

---
+++ app/test/test_pairing_app/android/app/src/main/kotlin/com/example/test_pairing_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_pairing_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app/test/test_pairing_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import flutter_ble_peripheral
+import network_info_plus
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterBlePeripheralPlugin.register(with: registry.registrar(forPlugin: "FlutterBlePeripheralPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
